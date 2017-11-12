5166b7e987292846ba658b442f3c0695c965c64d
Herbert Lausmann <herbert.lausmann@hotmail.com>
2017-11-12 06:56:09 -0300

    BUILD 260

--- HL.IconPro.Lib.Wpf/Motion/AnimatedCursorBitmapDecoder.cs
+++ HL.IconPro.Lib.Wpf/Motion/AnimatedCursorBitmapDecoder.cs
@@ -10,6 +10,15 @@ namespace HL.IconPro.Lib.Wpf.Motion
 {
     public class AnimatedCursorBitmapDecoder : Codec
     {
+        //For now, there is the need to implement methods and properties to get some metadata of the file like:
+        //The frame rate, size and etc
+        //The name and author;
+        //The hotspots...
+        //There are still some CHUNKS in the ANI file that I'm ignoring for now.
+        //For example, there is a chunk for the individual frame rate of each frame;
+        //There is a chunk data contains the sequence of the animation, for example 00:00{ frame1, frame4, frame2, frame3 ... }01:00
+        //So, these things are missing.
+
         public void Open(Stream Source)
         {
             //Note that, each frame of ani file might be a full (.cur) cursor file or a full (.ico) icon file or even
--- SVG.NET/Properties/AssemblyInfo.cs
+++ SVG.NET/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2.2.16.6525")]
+[assembly: AssemblyVersion("2.2.17.6525")]
 //[assembly: AssemblyFileVersion("1.0.1.*")]
 
 [assembly: CLSCompliant(true)]
