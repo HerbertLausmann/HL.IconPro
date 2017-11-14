6f62b442436098c2ccf0df512de471e8df6bd8f9
Herbert Lausmann <herbert.lausmann@hotmail.com>
2017-11-14 10:04:46 -0300

    BUILD 294

--- HL.IconPro.Lib.Wpf/Properties/AssemblyInfo.cs
+++ HL.IconPro.Lib.Wpf/Properties/AssemblyInfo.cs
@@ -50,5 +50,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("1.4.216.6527")]
-[assembly: AssemblyFileVersion("1.4.216.6527")]
+[assembly: AssemblyVersion("1.4.217.6528")]
+[assembly: AssemblyFileVersion("1.4.217.6528")]
--- HL.IconPro/Properties/AssemblyInfo.cs
+++ HL.IconPro/Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.293.6527")]
-[assembly: AssemblyFileVersion("1.2.293.6527")]
+[assembly: AssemblyVersion("1.2.294.6528")]
+[assembly: AssemblyFileVersion("1.2.294.6528")]
